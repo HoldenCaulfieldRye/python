45b5bde0134c7c3ead79bc32c71440add9ed9ebf
alex <adalyac@gmail.com>
2014-01-29 02:15:02 +0000

    started exercise

---
+++ exercises/ex1.py
@@ -0,0 +1,6 @@
+# http://web.archive.org/web/20100610043306/http://cse.ucdavis.edu/~chaos/courses/nlp/Homework/exercisesB.html
+
+# Write a program GetGA.py that uses the URL http://cse.ucdavis.edu/~chaos/courses/nlp/Homework/GettysburgAddress.txt to grab a text file containing the Gettysburg Address. Have the program write this to a file named ga.txt.
+
+def getGA(url, filename='ga.txt'):
+    
---
+++ pickle_explained.py
@@ -0,0 +1,54 @@
+
+# Pickle - aka seralize, marshal, flatten
+
+# serialization: process of translating data structures or object state
+# into a format that can be stored 
+
+# converts a python object hierarchy into a byte stream
+
+# unpickling converts a byte stream into an object hierarchy
+
+# converting to byte stream is needed for storing on disk or sending
+# over a network
+
+# pickle data format uses a printable ascii representation, so it is
+# human-readable with a text editor
+
+
+
+# Usage
+
+# to serialize an object hierarchy:
+# create a pickler
+# call pickler's dump(obj, file, protocol=0) method
+# byte stream will be written to file using protocol
+
+# to de-serialize:
+# create an unpickler
+# call unpickler's load(file) method
+
+
+
+# Protocols
+
+# version 0:
+# original ascii protocol, python backwards compatible
+
+# version 1:
+# old binary format, python backward compatible
+
+# version 2:
+# introduced in python-2.3, provides much more efficient pickling of
+# new-style classes
+# new-style classes: http://docs.python.org/2/glossary.html#term-new-style-class
+
+
+
+
+# cPickle
+
+# written in C, so can be up to 1000 times faster
+
+# does not support subclassing of Pickler and Unpickler classes
+# these are implemented as functions
+
