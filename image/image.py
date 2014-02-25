6a98a13adcb274404337f3f38955205e15bfbcc0
alex <adalyac@gmail.com>
2014-02-25 22:16:01 +0000

    cleaned directory, added image

---
+++ image/image.py
@@ -0,0 +1,13 @@
+# PIL: Python Imaging Library
+
+# http://effbot.org/imagingbook/image.htm
+
+# Load Image module
+from PIL import Image
+
+# Open jpg file
+im = Image.open('pickie.jpg')
+
+# Display image
+im.show() # think works
+
