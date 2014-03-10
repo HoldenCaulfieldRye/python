1825306629c4322c8f0ef8e2a5fa0146c33d2f0f
alex <adalyac@gmail.com>
2014-03-10 21:09:12 +0000

    nparray assigns by reference

--- image/image.py
+++ image/image.py
@@ -19,11 +19,16 @@ low_q_im = im.resize((width, height), Image.ANTIALIAS)
 # Save newly defined image to file in current directory
 low_q_img.save('imgName.jpg')
 
-# Create jpg image
-# trick is to start with an image and get its jpg
+# Create jpg image (from nparray)
+# trick is to start with an image and get its shape
+# btw, (I think) max rgb value is 255
 import numpy as np
-existingImg = Image.open('imgname.jpg')
-npimg = np.array(existingImg)
+existingImg = Image.open('imgname.jpg') # get jpg data
+monoNpImg = np.array(existingImg)       # in np format
+monoNpImg[:] = 0                        # make changes
+monoImg = Image.fromarray(monoNpImg)    # back into jpg
+monoImg.save('mono_1.jpg')              # save
+
 
 
 
--- numpy/numpy.py
+++ numpy/numpy.py
@@ -1,8 +1,36 @@
 import numpy as np
 
+
+# NUMPY ARRAYS COPY BY REFERENCE!!!
+
 # manually create array
 a = np.array([[1,4],[3,4],[2,5],[7,6]])
 
 # randomly create (big) array
 twoDarray = np.zeros([34,50], int)
 threeDarray = np.zeros([34,50,23], int)
+
+# modify a patch
+# CAREFUL!
+a[i:,j:,k] != a[i:][j:][k]
+# CAREFUL!
+a[i:,j:,k:] != a[i:][j:][k:]
+# a[i:,j:,k:] is all scalars a[x][y][z] st x>=i, y>=j, z>=k
+# a[i:][j:][k:] means take i-th and above subarrays, from this object take all j-th and above subarrays, from this object take all k-th and above subarrays.
+>>> b
+array([[ 0,  1,  2,  3],
+       [ 4,  5,  6,  7],
+       [ 8,  9, 10, 11],
+       [12, 13, 14, 15],
+       [16, 17, 18, 19],
+       [20, 21, 22, 23]], dtype=uint8)
+>>> b[1:,2:]
+array([[ 6,  7],
+       [10, 11],
+       [14, 15],
+       [18, 19],
+       [22, 23]], dtype=uint8)
+>>> b[1:][2:]
+array([[12, 13, 14, 15],
+       [16, 17, 18, 19],
+       [20, 21, 22, 23]], dtype=uint8)
