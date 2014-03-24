49972240a416cbe75fef32c08c9b434949135b52
alex <adalyac@gmail.com>
2014-03-24 16:13:34 +0000

    numpy matrix multiplication

--- numpy/numpy.py
+++ numpy/numpy.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 # NUMPY ARRAYS COPY BY REFERENCE!!!
 
 # manually create array
@@ -35,3 +34,7 @@ array([[12, 13, 14, 15],
        [16, 17, 18, 19],
        [20, 21, 22, 23]], dtype=uint8)
 
+
+# matrix multiplication: use dot
+>>> np.dot(array([(a,b),(c,d),(e,f)]),array([g,h]))
+array([a*g + b*h,  c*g + d*h,  e*g + f*h])
