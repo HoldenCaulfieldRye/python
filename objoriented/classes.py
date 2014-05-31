4e93b70197398ced572e82a654f3f87be1c8b792
alex <adalyac@gmail.com>
2014-05-31 17:59:19 +0200

    more about parallel, and classes

--- objoriented/classes.py
+++ objoriented/classes.py
@@ -1,63 +1,45 @@
 
-# Classes
-
+Classes:
 # multiple inheritance possible
 # derived class can override any base class method
 # classes are created at runtime
-
 # built-in types can be used as base classes for extension by the user
-
 # class members are PUBLIC by default
 # member functions are VIRTUAL by default
 
 
-
-# Pseudo Pointers
-
-# Multiple names can be bound to the same object, aka aliasing in other
-# languages. With mutable objects, aliases behave like pointers in some
+Pseudo Pointers:
+# Multiple names can be bound to the same object, aka aliasing in othe
+# languages. With mutable objects, aliases behave like pointers in som
 # respects.
-
 # Passing an object is cheap since only a pointer is passed by the
 # implementation; and if a function modifies an object passed as an
 # argument, the caller will see the change — this eliminates the need
 # for two different argument passing mechanisms as in Pascal.
-
 # "the caller will see the change" - ?
 
 
-
-# Namespaces
-
+Namespaces:
 # a namespace is a mapping from names to objects
-
 # they are implemented as dictionaries in python
-
 # there is no relation between names in different namespaces!
 # eg 2 modules may define maximise() - no confusion since python
 # requires to prefix by module name
-
 # the namespace containing all built-in names is created when python
 # interpreter starts up, and is never deleted
-
 # a module's namespace is created when module is imported, and is
 # usually deleted when interpreter quits.
-
-# namespace examples: built-in variable names, variable names local to
+# examples: built-in variable names, variable names local to
 # a module, variable names local to the body of a function
 
 
-
-# Inheritance
-
+Inheritance:
 # syntax for declaration
 class Subclass(BaseClass):
 
-
     
-# Class methods vs Instance methods
+Class methods vs Instance methods:
 # http://www.pythoncentral.io/difference-between-staticmethod-and-classmethod-in-python/
-
 def getNumberOfInstances(className):    # argument isn't a class object aka instance, but a class
     return className.no_inst
 
@@ -66,7 +48,6 @@ class Kls(object):
  
     def __init__(self):
         Kls.no_inst = Kls.no_inst + 1   # notice this is not a field, not an attribute! not self.no_inst, but Kls.no_inst
-
 
 obj = Kls()
 obj2 = Kls()
@@ -90,3 +71,18 @@ print Kls.getNumberOfInstances()        # 1 will be printed
 
 # python (unrigorous) flexibility: whether we call the method from the instance or the class, it passes the class as first argument
 
+
+Inherit from object:
+class A(object):
+...
+# same as (!)
+class A:
+...
+# it's just that a python new-style class always inherits from an
+# existing new-style class. object is just the null class.
+
+# more info:
+# bit.ly/1u0B2P8
+# why? exists since python 2.2 as solution to previous design flaws.
+# flaw: could not derive a class from a primitive (built-in) type.
+#       eg could not subclass a list to add a useful method to it.
--- parallel/joblib-parallel.py
+++ parallel/joblib-parallel.py
@@ -1,20 +1,33 @@
 # https://pythonhosted.org/joblib/parallel.html
+# http://bit.ly/1kSdgBR
 
-# Parallel is a class to parallelise for-loops using multiprocessing.
-# delayed is a function for creating a tuple (function, args, kwargs)
+
+Parallel:
+# a class to parallelise for-loops using multiprocessing.
+
+delayed:
+# a function for creating a tuple (function, args, kwargs)
 # with a functional (?) syntax
 
-# WOAH! it won't parallelise on a multi-core processor!
-# or maybe it can? http://bit.ly/1kSdgBR
+kwargs:
+# modules to import?  
 
-# eg:
+example:
 from math import sqrt
+from joblib import Parallel, delayed
 [sqrt(i ** 2) for i in range(10)]
 # becomes, for 3 CPU cores:
-from joblib import Parallel, delayed
 Parallel(n_jobs=3)(delayed(sqrt)(i ** 2) for i in range(10))
-
 
 # under the hood: Parallel object forks the python interpreter in
 # multiple processes to execute each of the items of the list,
 # concurrently, on separate cores.
+
+
+multi{ threads vs processes }:
+# If you know that the function you are calling is based on a compiled extension that releases the Python Global Interpreter Lock (GIL) during most of its computation then it might be more efficient to use threads instead of Python processes as concurrent workers.
+
+
+parallel IO:
+# http://stackoverflow.com/questions/22755218/parallel-i-o-why-does-it-work
+# http://stackoverflow.com/questions/23970655/parallelise-an-io-heavy-for-loop-stupid-idea
